6773d2cf2481d01ff7ee03e67ded0e5eaa54d8fe
devock <ockjungbong@gmail.com>
2023-10-11 00:30:11 +0900

    chunk 처리 추가

--- src/main/java/com/example/batch/job/TestChunkJob/TestChunkJobConfig.java
+++ src/main/java/com/example/batch/job/TestChunkJob/TestChunkJobConfig.java
@@ -11,6 +11,10 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.support.ListItemReader;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -34,7 +38,8 @@ public class TestChunkJobConfig {
     public Job testTaskChunkJob() {
         return jobBuilderFactory.get("testTaskChunkJob")
                 .incrementer(new RunIdIncrementer())
-                .start(this.taskStep1())
+                .start(this.taskStep1()) // tasklet 처리
+                .next(this.chunkStep1()) // chunk 처리
                 .build();
     }
 
@@ -66,6 +71,29 @@ public class TestChunkJobConfig {
         }
 
         return items;
+    }
+
+    @JobScope
+    @Bean
+    public Step chunkStep1() { // chunksize로 처리
+        return stepBuilderFactory.get("chunkStep1")
+                .<String, String>chunk(10)
+                .reader(itemReader())
+                .processor(itemProcessor())
+                .writer(itemWriter())
+                .build();
+    }
+
+    private ItemReader<String> itemReader() {
+        return new ListItemReader<>(getItems());
+    }
+
+    private ItemProcessor<String, String> itemProcessor() {
+        return item -> item + " now processor!!";
+    }
+
+    private ItemWriter<String> itemWriter() {
+        return items -> log.info("### writer : " + items.toString());
     }
 
 }
